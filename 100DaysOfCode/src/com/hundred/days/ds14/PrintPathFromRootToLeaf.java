3ef7334b9b4e810999fc059482a1c82ea9af0b08
purusottam kumar <chaks.honor@gmail.com>
2021-06-26 20:28:37 +0530

    2nd commit

--- 100DaysOfCode/src/com/hundred/days/ds14/PrintPathFromRootToLeaf.java
+++ 100DaysOfCode/src/com/hundred/days/ds14/PrintPathFromRootToLeaf.java
@@ -1,5 +1,7 @@
 package com.hundred.days.ds14;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -9,24 +11,25 @@ import com.hundred.days.ds13.TreeUtils;
 public class PrintPathFromRootToLeaf {
 	public static void main(String[] args) {
 		TreeNode root = TreeUtils.getTree();
-		Queue<Integer>  q = new LinkedList<>();
+		Deque<Integer>  q = new ArrayDeque<Integer>();
 		printThePath(root,q);
 	}
 
-	private static void printThePath(TreeNode root,Queue<Integer>  q) {
+	private static void printThePath(TreeNode root,Deque<Integer>  q) {
 		
 		if(root==null) {
 			return;
 		}
 		
+		q.addLast(root.data);
 		if(root.left==null && root.right==null) {
-			
+			System.out.println(q);
 		}
 		
 		printThePath(root.left,q);
 
 		printThePath(root.right,q);
-		
+		q.removeLast();
 		
 		
 	}
